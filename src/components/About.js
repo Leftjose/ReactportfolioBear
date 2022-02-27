37cd5dd98163908ddcc4470a94024aba9acece16
Jose Izquierdo <leftjose@gmail.com>
2022-02-26 22:45:34 -0600

    editing the text from about me and prjects

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,11 @@ export default function About() {
             Developer.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            Greetings, I am currently looking for a Internship or a Entry Level Fullstack Web Developer role. 
+            I completed a Full Stack Web Developer Bootcamp at UTSA in January 2022. 
+            You are welcome to contact me if you have any further questions. <br>
+            </br>
+            Thank You 
           </p>
           <div className="flex justify-center">
             <a
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,9 +13,7 @@ export default function Projects() {
              Apps I've Built
            </h1>
            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-             facilis repellat ab cupiditate alias vero aliquid obcaecati
-             quisquam fuga dolore.
+             Here are some of the Web applications I have created and worked on with a team of developers from the Bootcamp.
            </p>
          </div>
          <div className="flex flex-wrap -m-4">
